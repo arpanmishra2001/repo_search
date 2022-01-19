be81fcb03727dd73c8d3f3baf11014785f1806d4
ARPAN MISHRA <52422185+arpanmishra2001@users.noreply.github.com>
2022-01-19 21:43:37 +0530

    Add files via upload

---
+++ Git-Hub_repo_search-master/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.datafrominternet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO (26) Create an EditText variable called mSearchBoxEditText
+
+    // TODO (27) Create a TextView variable called mUrlDisplayTextView
+    // TODO (28) Create a TextView variable called mSearchResultsTextView
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+
+        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
+        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+    }
+}
